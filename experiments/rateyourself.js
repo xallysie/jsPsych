99c24cacaaf2fee95c2f35fc853e46deb873c7e5
Sally Xie <48843867+xallysie@users.noreply.github.com>
2023-03-04 03:11:23 -0500

    Add files via upload

--- experiments/rateyourself.js
+++ experiments/rateyourself.js
@@ -1,5 +1,6 @@
 /* initialize jsPsych */
 var jsPsych = initJsPsych({
+    show_progress_bar: true,
     /* uncomment below to debug test data collection */
     //on_finish: function() {
     //    jsPsych.data.displayData();
